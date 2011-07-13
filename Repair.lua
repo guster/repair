704bd089b70623e546b976893c5b8904c2d833e7
Guster <gustertheshaman@gmail.com>
2011-07-13 08:55:53 -0700

    Use Guild Bank Repairs if available and report money spent in the chat frame.

--- Repair.lua
+++ Repair.lua
@@ -1,5 +1,14 @@
 local f = CreateFrame("Frame")
-f:RegisterEvent("MERCHANT_SHOW")
 f:SetScript("OnEvent", function ()
-  if CanMerchantRepair() then RepairAllItems() end
+  local cost, canRepair = GetRepairAllCost()
+  
+  if canRepair then
+    local canGuildRepair = CanGuildBankRepair()
+    local moneySpent = GetCoinText(cost)..(canGuildRepair and " (Guild)" or "")
+    
+    RepairAllItems(canGuildRepair)
+    
+    ChatFrame1:AddMessage("|cff88ccddRepair:|r Spent "..moneySpent.." on repairs.")
+  end
 end)
+f:RegisterEvent("MERCHANT_SHOW")

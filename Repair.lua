a368b11db94ab586f5d7a4f97f26fe378335e8c8
Guster <gustertheshaman@gmail.com>
2011-07-15 09:01:35 -0700

    Improved guild bank logic.

--- Repair.lua
+++ Repair.lua
@@ -4,11 +4,19 @@ f:SetScript("OnEvent", function ()
   
   if canRepair then
     local canGuildRepair = CanGuildBankRepair()
-    local moneySpent = GetCoinText(cost)..(canGuildRepair and " (Guild)" or "")
     
-    RepairAllItems(canGuildRepair)
+    if canGuildRepair and cost > GetGuildBankWithdrawMoney() then
+      canGuildRepair = nil
+    end
     
-    ChatFrame1:AddMessage("|cff88ccddRepair:|r Spent "..moneySpent.." on repairs.")
+    if canGuildRepair == nil and cost > GetMoney() then
+      ChatFrame1:AddMessage("|cff88ccddRepair:|r Not enough money to repair.")
+    else
+      RepairAllItems(canGuildRepair)
+    
+      local moneySpent = GetCoinText(cost)..(canGuildRepair and " (Guild)" or "")
+      ChatFrame1:AddMessage("|cff88ccddRepair:|r Spent "..moneySpent.." on repairs.")
+    end
   end
 end)
 f:RegisterEvent("MERCHANT_SHOW")
